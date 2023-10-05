49a461da8998e43b79e2d3688a3f9ec531bd8d84
Raghu1426 <113841807+Raghu1426@users.noreply.github.com>
2023-10-05 20:42:15 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class sum
+{
+	public static void main(String args[])
+	{
+		float a=2,b=6,c=9,d=34,e=26,avg,sum;
+		sum=a+b+c+d+e;
+		avg=sum/5;
+		System.out.println("Sum of 5 numbers : "+sum);
+		System.out.println("Average of 5 numbers : "+avg);
+	}
+}
